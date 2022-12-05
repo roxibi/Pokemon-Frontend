f5b406d5e6092e3265e795d73dd843fab10c9c0b
Nizami Suleymanov <nizami.suleymanoff@gmail.com>
2022-12-05 17:55:23 +0100

    added images of all pokemons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState, } from "react";
+import { useState } from "react";
 import React from "react";
-import Cards from './Cards';
+import Cards from './components/Cards';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
 <h1>PokeFight</h1>
 <div><Cards/></div>
     </div>
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='headerSec'>
+        <ul>
+            <h1>Welcome to PokeFight</h1>
+        <a>Home</a>
+        <a>Pokemons</a>
+        <a>FAQ</a>
+        <a>Contact</a>
+        </ul>
+    </div>
+  )
+}
+
+export default Header
