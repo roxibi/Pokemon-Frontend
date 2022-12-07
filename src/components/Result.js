3319f975529c3672c64748a11a8982a268164aac
roxi <you@example.com>
2022-12-07 14:02:39 +0100

    displaying pokedata in the fight section

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,12 +3,15 @@ import React from 'react'
 function Result ({ p1Pokemon, p2Pokemon })  {
     p1Pokemon? console.log(p1Pokemon) :  console.log('no pokemon')
     p2Pokemon? console.log(p1Pokemon) :  console.log('no pokemon')
-    
+    const winner=()=>{
+        if (p1Pokemon.base.HP-p2Pokemon.base.Attack>p2Pokemon.base.HP-p1Pokemon.base.Attack) {return  p1Pokemon.name.english}
+    }
   return (
     p1Pokemon, p2Pokemon && 
     <div>
-      { p1Pokemon.name.english}  -----VS-----      
-      { p2Pokemon.name.english}
+        <></>
+     <div>{ p1Pokemon.name.english}</div>   -----VS-----      
+     <div>{ p2Pokemon.name.english}</div> 
       {p1Pokemon.base.HP}
     </div>
   )
